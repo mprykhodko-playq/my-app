d45b1549f0ee76ee0184483c8520b79782f81c36
mikeprikhodko <mprykhodko@playq.net>
2019-04-09 16:28:06 +0300

    new project for react testing

---
+++ src/couter/counter.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+    }
+
+    increment = () => this.setState(prevState => {
+            return {
+                count: prevState.count + 1
+            }
+        }
+    );
+
+    render() {
+        return(
+            <div>
+                <p>Current count: {this.state.count}</p>
+                <button onClick={this.increment}>Increment Count</button>
+            </div>
+        )
+    }
+}
+
+export default Counter
